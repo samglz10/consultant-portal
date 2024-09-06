3d46290914c358ce88572c02585d9dfa7222a3e8
Samuel Gonzalez <samglz10@gmail.com>
2024-09-05 21:10:00 -0400

    updated with company name

--- src/assets/HomePage.jsx
+++ src/assets/HomePage.jsx
@@ -9,7 +9,7 @@ const Home = ()=>{
     <NavBar style={{ position: 'sticky'}}/>
     <header id="home">
         <div className="container">
-                <h1>Freelance Consultant</h1>
+                <h1>NovusPaths LLC</h1>
                 <h2>Providing Consultation Solutions for you</h2>
                 <p>Helping you achieve your goals with technology and expertise.</p>
         </div>
@@ -17,14 +17,27 @@ const Home = ()=>{
     <section id="about" className="container">
         <h2> <FcAbout/> About Me</h2>
         <div className="box">
-            <p>As a freelance consultant, I specialize in helping businesses streamline their operations through effective use of technology. I offer a range of services designed to meet your unique needs.</p>
+            <p> With a robust background in providing support to various US. government agencies, including the FBI, DOS, and DOJ, I am a highly skilled Management Analyst and Management Program Analyst. 
+                My career has been defined by my ability to perform in-depth analysis and managementof complex case reviews and data entry tasks.
+            </p>
+            <p>
+                My current role requires a deep understanding of application procedures, 
+                the ability to interpret complex laws, and the flexibility to handle inquiries from various stakeholders, including foreign diplomatic missions and US. government agencies. 
+                I have provided full-service support, conducted research, and mentored new hires. 
+            </p>
+            <p>
+                My experience also includes roles in fraud investigation, background checks, and customer support in security system integration.
+                I hold a Bachelor’s degree in Criminal Justice with a minor in Spanish, and I have further enhanced my technical skills through a Software Development Professional Program at American University. 
+                I am proficient in multiple software platforms, including Microsoft Office, Power Bi, Power Automate, Adobe Illustrator, and various web development technologies. 
+                I am also fluent in Spanish, I bring a versatile skill set and a commitment to excellence in all my professional endeavors.
+            </p>
         </div>
     </section>
     <section id="services" className="container">
         <h2><FcSelfServiceKiosk/> Services</h2>
         <div className="box">
             <h3>IT Consulting</h3>
-            <p>Providing expert advice on technology solutions that fit your business.</p>
+            <p>Providing expert advice on technology solutions that fit your small business needs.</p>
         </div>
         <div className="box">
             <h3>Network Solutions</h3>
--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -2,7 +2,8 @@
 const NavBar = ()=>{
     return(
         <nav>
-            <a href="#home">Home</a>
+    
+            <a href="#home">NovusPaths LLC</a>
             <a href="#about">About</a>
             <a href="#services">Services</a>
             <a href="#contact">Contact</a>
